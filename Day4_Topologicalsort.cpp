12e951c033351d601404a5cc0036575d15eb9409
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2020-09-20 21:46:57 +0530

    Update Day4_Topologicalsort.cpp

--- Day4_Topologicalsort.cpp
+++ Day4_Topologicalsort.cpp
@@ -7,6 +7,7 @@ class Graph{
      list<int> *adj;
      Graph(int V);
      void topologicalSort();
+    void topologicalSortKahn();
      void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
      void addEdge(int u,int v);
 };
@@ -27,6 +28,49 @@ void Graph::topologicalSortUtil(int v,bool visited[],stack<int>&Stack){
     }
     //push vertex to the stack
     Stack.push(v);
+}
+//with kahn's algo
+void Graph::topologicalSortKahn(){
+   //vector to store indegrees 
+   vector<int> inDegree(V,0);
+   for(int u=0;u>V;u++){
+       list<int>::iterator it;
+       for(it = adj[u].begin();it!=adj[u].end();++it){
+           inDegree[*it]++;
+       }
+   }
+   //queue to store vertices with indegreee 0
+   queue<int> Q;
+   for(int i=0;i<V;i++){
+       if(inDegree[i]==0){
+           Q.push(i);
+       }
+   }
+   int count=0;
+   vector<int>TopOrder;
+   while(!Q.empty()){
+       int front = Q.front();
+       Q.pop();
+       TopOrder.push_back(front);
+       // decrease its neighbour's indegree by one 
+       //because we have completed that task 
+       list<int>::iterator it;
+       for(it = adj[front].begin();it!=adj[front].end();++it){
+           if(--inDegree[*it] == 0){
+               Q.push(*it);
+           }
+       }
+       count++;
+   }
+   if(count!=V){
+       cout<<"Cycle detected"<<endl;
+       return;
+   }
+   for(auto i : TopOrder){
+       cout<<i<<" ";
+   }
+   cout<<endl;
+    
 }
 
 void Graph::topologicalSort(){
