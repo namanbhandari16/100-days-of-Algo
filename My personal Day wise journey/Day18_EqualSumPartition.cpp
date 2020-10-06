856b19c75c2e0f0c63b8cd18adcfa031a3619555
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2020-10-06 20:51:18 +0530

    Update Day18_EqualSumPartition.cpp

--- My personal Day wise journey/Day18_EqualSumPartition.cpp
+++ My personal Day wise journey/Day18_EqualSumPartition.cpp
@@ -1,53 +1,26 @@
-// A Dynamic Programming based 
-// C++ program to partition problem 
-// thanks to gfg
-#include <bits/stdc++.h> 
-using namespace std; 
-
-// Returns true if arr[] can be partitioned 
-// in two subsets of equal sum, otherwise false 
-bool findPartiion (int arr[], int n) 
-{ 
-	int sum = 0; 
-	int i, j; 
-
-	for (i = 0; i < n; i++) 
-	sum += arr[i]; 
-	
-	if (sum % 2 != 0) 
-	return false; // not possible if total sum is odd
-	
-	bool DP[sum / 2 + 1][n + 1]; 
-    //if sum is zero then it is possible {empty set}
-	for (i = 0; i <= n; i++) 
-	  DP[0][i] = true; 
-    //if n is zero then it is not possible 
-    // remember base case  in recursion
-	for (i = 1; i <= sum / 2; i++) 
-	  DP[i][0] = false; 
-
-	for (i = 1; i <= sum / 2; i++) 
-	{ 
-		for (j = 1; j <= n; j++) 
-		{ 
-			DP[i][j] = DP[i][j - 1]; 
-			if (i >= arr[j - 1]) 
-			DP[i][j] = DP[i][j] || 
-						DP[i - arr[j - 1]][j - 1]; 
-		}	 
-	} 
-	return DP[sum / 2][n]; 
-} 
-
-
-int main() 
-{ 
-	int arr[] = {3, 1, 1, 2, 2, 1}; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-	if (findPartiion(arr, n) == true) 
-		cout << "YES"; 
-	else
-		cout << "NO";
-	return 0; 
-} 
-
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+       int totalSum = 0;
+       for(int i=0;i<N;i++){
+           totalSum+=arr[i];
+       }
+       if(totalSum%2==1)return 0; // not possible for odd total sum
+       bool dp[(totalSum)/2+1][N+1];
+       //if n == 0 then its false
+       for(int i=0;i<=totalSum/2;i++)dp[i][0] = false;
+       for(int i=0;i<=N;i++)dp[0][i] = true;
+       
+       for(int i=1;i<=totalSum/2;i++){
+           for(int j=1;j<=N;j++){
+              
+               if(arr[j-1]<=i){
+                   dp[i][j] = dp[i][j-1] || dp[i - arr[j-1]][j-1];
+               }else  dp[i][j] = dp[i][j-1];
+           }
+       }
+       return dp[totalSum/2][N];
+       
+    }
+};
